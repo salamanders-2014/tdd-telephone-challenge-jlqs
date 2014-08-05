e0e7db334c56a151062ad01999523de953e1f441
Quy Tran & Simon Gondeck <pair+quytx1102.simongondeck@devbootcamp.com>
2014-08-05 16:25:39 -0500

    done

--- pizza/pizza.rb
+++ pizza/pizza.rb
@@ -1,11 +1,13 @@
+require_relative 'topping'
 class Pizza 
-	attr_reader :name, :description, :time_baked, :toppings
+	attr_reader :name, :description 
+	attr_accessor :toppings, :time_baked
 
 	def initialize(args = {})
 		@name = args[:name]
 		@description = args[:description] || "Fat"
 		@time_baked = args[:time_baked] || 0
-		@toppings = []
+		@toppings = Array.new
 	end
 
 	def name
@@ -13,6 +15,20 @@ class Pizza
 		@name
 	end
 
+	def bake(time)
+		self.time_baked += time
+		@toppings.each {|t| t.time_baked += time }
+	end
+
+
+	def add_toppings(toppings)
+		toppings.each do |t|
+			self.toppings << t
+		end
+	end
+
+	def required_bake_time 
+		@toppings.reduce(900) { |sum, t| sum + t.required_bake_time }
+	end
+
 end
-
-
