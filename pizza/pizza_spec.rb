3f4ebdf56c57fa32a4d34d43592f968821b4e02c
Jeremy & Lienha Carleton <pair+jermspeaks.lienha@devbootcamp.com>
2014-08-05 10:14:22 -0500

    fixed some test

--- pizza/pizza_spec.rb
+++ pizza/pizza_spec.rb
@@ -4,31 +4,34 @@ require_relative 'pizza'
 describe Pizza do
   describe 'attributes' do
     before do
-      @pizza = Pizza.new
+      @pizza = Pizza.new(name: pepperoni)
     end
 
     context '#name' do
       it 'should have a name' do
-        expect(pizza.name).to be_a String
+        expect(@pizza.name).to be_a String
       end
     end
 
     context '#description' do
       it 'should have a description' do
-        expect(pizza.description).to be_a String
+        expect(@pizza.description).to be_a String
       end
     end
 
     context '#time_baked' do
       it 'should have a time_baked' do
-        expect(pizza.time_baked).to be_a Integer
+        expect(@pizza.time_baked).to be_a Integer
       end
     end
   end
 
   describe 'intialize' do
-    it 'should be initialized with a name' do
-
+    context 'should be initialized with a name' do
+      it 'should raise an error if no name is given' do
+        pizza = Pizza.new
+        expect(pizza.name).to raise_error(ArgumentError)
+      end
     end
   end
 
