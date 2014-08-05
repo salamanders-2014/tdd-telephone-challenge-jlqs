e5e47aeec8aca06b91e480a231fda36ce557e57b
Jeremy & Lienha Carleton <pair+jermspeaks.lienha@devbootcamp.com>
2014-08-05 10:20:09 -0500

    added time_baked test

--- pizza/pizza_spec.rb
+++ pizza/pizza_spec.rb
@@ -4,12 +4,13 @@ require_relative 'pizza'
 describe Pizza do
   describe 'attributes' do
     before do
-      @pizza = Pizza.new(name: pepperoni)
+      @pizza = Pizza.new(name: "pepperoni")
     end
 
     context '#name' do
       it 'should have a name' do
         expect(@pizza.name).to be_a String
+        expect(@pizza.name).to eq("pepperoni")
       end
     end
 
@@ -26,11 +27,17 @@ describe Pizza do
     end
   end
 
-  describe 'intialize' do
-    context 'should be initialized with a name' do
+  describe '#initialize' do
+    context '#name' do
       it 'should raise an error if no name is given' do
         pizza = Pizza.new
         expect(pizza.name).to raise_error(ArgumentError)
+      end
+    end
+    context '#time_baked' do
+      it 'should default to 0' do
+        pizza = Pizza.new(name: "pepperoni")
+        expect(pizza.time_baked).to eq(0)
       end
     end
   end
