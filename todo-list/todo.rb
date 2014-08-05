5caa03c07b6ec56d92787e775df9fdd420728c5e
Jeremy & Lienha Carleton <pair+jermspeaks.lienha@devbootcamp.com>
2014-08-05 15:26:58 -0500

    passed new methods tasks

--- todo-list/todo.rb
+++ todo-list/todo.rb
@@ -1,3 +1,5 @@
+require_relative 'task'
+
 class Todo
   attr_reader :title, :tasks
   def initialize(args={})
@@ -7,6 +9,22 @@ class Todo
 
   def add_task(task)
     @tasks.push(task)
+  end
+
+  def complete_all!
+    @tasks.each { |task| task.mark_as_complete! }
+  end
+
+  def complete?
+    @tasks.all? { |t| t.complete? }
+  end
+
+  def completed_tasks
+    @tasks.select { |task| task.complete? }
+  end
+
+  def incompleted_tasks
+    @tasks.reject { |task| task.complete? }
   end
 
 end
