0e0037e2352a92999f0d24df8e5807799f2baa25
quytx1102 <quytx1102@gmail.com>
2014-08-05 15:01:26 -0500

    2-todo spec

--- todo-list/todo_spec.rb
+++ todo-list/todo_spec.rb
@@ -1,15 +1,33 @@
 require 'rspec'
 require_relative 'todo'
+require_relative 'task'
 
 describe Todo do
 
   before do
     @todo = Todo.new(title: "test")
+    @t1 = Task.new(name: "Get a beer", description: "Corona Light")
+    @t2 = Task.new(name: "Go for a walk", description: "After this challenge")
   end
 
   describe '#title' do
     it 'should have a title' do
       expect(@todo.title).to eq("test")
+    end
+  end
+
+  describe '#tasks' do
+    it 'should have many tasks' do
+      expect(@todo.tasks.length).to be_an Integer
+    end
+  end
+
+  describe '#add_task' do
+    it "should be able to add task" do
+      @todo.add_task(@t1)
+      expect(@todo.tasks.length).to eq(1)
+      @todo.add_task(@t2)
+      expect(@todo.tasks.length).to eq(2)
     end
   end
 
