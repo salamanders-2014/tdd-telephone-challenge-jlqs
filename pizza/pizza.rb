da6168a9f0d5673f2c609e3b928dd0fac7c7628c
Quy Tran & Simon Gondeck <pair+quytx1102.simongondeck@devbootcamp.com>
2014-08-05 14:29:41 -0500

    next

--- pizza/pizza.rb
+++ pizza/pizza.rb
@@ -1,10 +1,11 @@
 class Pizza 
-	attr_reader :name, :description, :time_baked
+	attr_reader :name, :description, :time_baked, :toppings
 
 	def initialize(args = {})
 		@name = args[:name]
 		@description = args[:description] || "Fat"
 		@time_baked = args[:time_baked] || 0
+		@toppings = []
 	end
 
 	def name
